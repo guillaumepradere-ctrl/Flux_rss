375c1f2de37d6eccaf08934b384ad175a1f77e04
GuillaumeP <guillaume.pradere@newheat.fr>
2026-03-27 16:37:34 +0100

    Lecture en direct du fichier Sharepoint

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the Excel file
-df = pd.read_excel(r'C:\Developpement\Flux_rss\Comment.xlsx')
+df = pd.read_excel(r"C:\Users\g.pradere\NEWHEAT\00_EXPLOITATION - Documents\General\231218_Exploit_Suivi_Centrales.xlsm", sheet_name='Form1')
 
 # Save as CSV in the same folder
 df.to_csv(r'C:\Developpement\Flux_rss\Comment.csv', index=False)
